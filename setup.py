04b9932e863e89778d1794e1657f81acf4cce9b5
Marcin Bachry <hegel666@gmail.com>
2021-12-08 18:57:11 +0100

    foo

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
     'Operating System :: POSIX',
     'Programming Language :: C',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
